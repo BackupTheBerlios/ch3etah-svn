1c01763f999463623cb564941b850bcbfcc39ac8
igoravl <igoravl@46a6ea6d-f202-0410-8de6-c1397b24a2cf>
2005-10-14 02:41:17 +0000

    Adjusted assembly version

--- trunk/src/Ch3Etah.Core/AssemblyInfo.cs
+++ trunk/src/Ch3Etah.Core/AssemblyInfo.cs
@@ -1,11 +1,5 @@
+using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following
-// attributes.
-//
-// change them to the information which is associated with the assembly
-// you compile.
 
 [assembly: AssemblyTitle("CH3ETAH Core Library")]
 [assembly: AssemblyDescription("Provides the core functionality for the CH3ETAH Project.")]
@@ -16,17 +10,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all values by your own or you can build default build and revision
-// numbers with the '*' character (the default):
-
-[assembly: AssemblyVersion("0.40.*")]
-
-// The following attributes specify the key for the sign of your assembly. See the
-// .NET Framework documentation for more information about signing.
-// This is not required, if you don't want signing let these attributes like they're.
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
+#if DEBUG
+[assembly : AssemblyVersion("0.1.0.*")]
+#else
+[assembly : AssemblyVersion("0.5.2005.1013")]
+//[assembly : AssemblyKeyFile("..\\..\\..\\Ch3Etah.snk")]
+#endif
