3883fd252c2ffd63098208618aa066d67af37aec
jacobmcp <jacobmcp@46a6ea6d-f202-0410-8de6-c1397b24a2cf>
2006-02-02 21:33:06 +0000

    CHG: Changed build files to clear old log files before building.

--- trunk/src/_BUILD_ALL.bat
+++ trunk/src/_BUILD_ALL.bat
@@ -1,3 +1,5 @@
+del "libraries.build.log"
+cls
 CALL "%VS71COMNTOOLS%"\vsvars32.bat
 DevEnv.com "Libraries\Ch3Etah.Libraries.sln" /build Debug /nologo /out "libraries.build.log"
 REM DevEnv.com "Ch3Etah.sln" /rebuild Debug /nologo
--- trunk/src/_BUILD_CODESMITH_SUPPORT.bat
+++ trunk/src/_BUILD_CODESMITH_SUPPORT.bat
@@ -1,3 +1,5 @@
+del "ch3etah.build.log"
+cls
 CALL "%VS71COMNTOOLS%"\vsvars32.bat
 DevEnv.com "Ch3Etah.CodeSmithSupport.sln" /rebuild Debug /nologo /out "ch3etah.build.log"
 makensis /DPRODUCT_VERSION=0.5.2 Installer\Ch3Etah.Installer.nsi
